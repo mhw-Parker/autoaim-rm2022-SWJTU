2e38fd443c5ca95cc12c8021b6c8cc795f9f6943
TianYY <62873029+mhw-Parker@users.noreply.github.com>
2021-10-05 18:39:12 +0800

    Add files via upload

---
+++ Drivers/MindVision/include/MindVision.h
@@ -0,0 +1,45 @@
+//
+// Created by tyy on 2021/9/28.
+//
+
+#ifndef MINDVISION_MINDVISION_H
+#define MINDVISION_MINDVISION_H
+
+#include "CameraApi.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc/imgproc_c.h>
+#include <iostream>
+#include "Driver.h"
+
+
+using namespace std;
+using namespace cv;
+
+class MindVision : public Driver
+{
+public:
+    bool InitCam() override;
+    bool SetCam() override;
+    bool StartGrab() override;
+    bool Grab(Mat& src) override;
+    bool StopGrab() override;
+
+private:
+    int                     hCamera;
+    int                     iCameraCounts = 1;
+    int                     iStatus=-1;
+    int                     fps;
+    tSdkCameraDevInfo       tCameraEnumList;
+    tSdkCameraCapbility     tCapability;      //设备描述信息
+    tSdkFrameHead           sFrameInfo;
+    BYTE*			        pbyBuffer;
+    int                     iDisplayFrames = 10000;
+    IplImage *iplImage = NULL;
+    double*         pfLineTime;
+    int                     channel = 3;
+    unsigned char           * g_pRgbBuffer;     //处理后数据缓存区
+};
+
+#endif //MINDVISION_MINDVISION_H
---
+++ Drivers/VideoDriver/src/VideoDriver.cpp
@@ -0,0 +1,39 @@
+//
+// Created by root on 2021/3/6.
+//
+#include "VideoDriver.hpp"
+
+bool VideoDriver::InitCam()
+{
+    capture.open(videoPath);
+    if(!capture.isOpened())
+    {
+        perror("Video Open Failed!\n");
+        return false;
+    }else{
+        LOGM("Video Open Successfully!\n");
+    }
+    return true;
+}
+bool VideoDriver::StartGrab()
+{
+    return true;
+}
+bool VideoDriver::SetCam()
+{
+    return true;
+}
+bool VideoDriver::Grab(Mat& src)
+{
+    //printf("Video Capture\n");
+    capture.read(src);
+    if(src.empty()) printf("grab failed !");
+//    capture.set(CAP_PROP_FRAME_WIDTH, 1024);			//设置相机采样宽度
+//    capture.set(CAP_PROP_FRAME_HEIGHT, 820);		//设置相机采样高度
+    return !src.empty();
+}
+
+bool VideoDriver::StopGrab()
+{
+
+}
