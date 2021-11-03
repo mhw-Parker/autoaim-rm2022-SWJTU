a2aeff5e640fcc0cd3dbd46afad3c55a5e085e4d
TianYY <62873029+mhw-Parker@users.noreply.github.com>
2021-11-03 20:43:53 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,33 +12,9 @@ pthread_t detectPThreadHandler;
 pthread_t energyPThreadHandler;
 pthread_t feedbackPThreadHandler;
 
-#if SAVE_VIDEO == 1
-int video_save_count;
-#endif
-
 int main(int argc, char** argv)
 {
     PreOptions(argc,argv);
-
-#if SAVE_VIDEO == 1
-    std::ifstream fileVideoCountRead("../Log/video_count_file.txt", ios::in);
-    if(!fileVideoCountRead.is_open())
-    {
-        LOGE("VIDEO SAVE FAILED\n");
-        video_save_count = 0;
-    }
-    else
-    {
-        fileVideoCountRead >> video_save_count;
-    }
-    fileVideoCountRead.close();
-
-    std::ofstream fileVideoCountWrite("../Log/video_count_file.txt", ios::out);
-    if(!fileVideoCountWrite.is_open())
-        LOGE("VIDEO SAVE FAILED\n");
-    fileVideoCountWrite << video_save_count + 1 << endl;
-    fileVideoCountWrite.close();
-#endif
 
     ImgProdCons pro;
     if(pro.Init()){
