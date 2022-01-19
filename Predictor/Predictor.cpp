d7c1a1446265156e6f8a0162cba6222e26f03fae
tyy <tshuai1410771022@gmail.com>
2022-01-19 23:31:05 +0800

    refresh some docs

--- Predictor/Predictor.cpp
+++ Predictor/Predictor.cpp
@@ -4,12 +4,19 @@
 #include "Predictor.h"
 
 void Predictor::armorPredictor(Vector3f p_cam_xyz, const float deltaT) {
+    predict_point.clear();
     target_xyz.push_back(Point3_<float>(p_cam_xyz[0], p_cam_xyz[1], p_cam_xyz[2]));
     if(target_xyz.size()){
         Point3_<float> v_xyz = (target_xyz.back() - target_xyz[target_xyz.size()-4]) / (deltaT * 3);
-        predict_xyz = target_xyz.back() + v_xyz * 200;
-
-        cout << p_cam_xyz << " " << predict_xyz << endl;
+        Point3f out_xyz = target_xyz.back() + v_xyz * 200;
+        predict_xyz[0] = out_xyz.x;
+        predict_xyz[1] = out_xyz.y;
+        predict_xyz[2] = out_xyz.z;
+        //cout << p_cam_xyz << " " << predict_xyz << endl;
     }
+}
+
+void Predictor::test1Predict(Vector3f pyd, const float deltaT) {
+
 
 }
