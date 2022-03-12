651752ff49e5d6e1827afec54b96d26fb256c153
tyy <tshuai1410771022@gmail.com>
2022-03-12 22:42:16 +0800

    refresh some docs

--- Math/Kalman/Kalman.cpp
+++ Math/Kalman/Kalman.cpp
@@ -2,10 +2,15 @@
 
 Kalman::Kalman() {}
 
-void Kalman::Init(MatrixXd A_in, MatrixXd Q_in, MatrixXd R_in) {
-    A_ = A_in;
-    Q_ = Q_in;
-    R_ = R_in;
+void Kalman::Init(int z_dim, int x_dim, float step) {
+    H_ = MatrixXd::Identity(z_dim,x_dim); //初始化为 z_dim * x_dim 的标准矩阵
+    A_ = MatrixXd::Identity(x_dim,x_dim); //初始化为 x_dim * x_dim 的标准矩阵
+    if(x_dim % z_dim == 0){
+        for(int i = 0; i < x_dim/2; i++){
+            A_(i,z_dim + i) = step;
+        }
+    }
+    P_ = MatrixXd::Identity(x_dim, x_dim);
 }
 
 void Kalman::Update(VectorXd z_k) {
@@ -14,24 +19,24 @@ void Kalman::Update(VectorXd z_k) {
     //k时刻先验估计协方差
     MatrixXd P_bar = A_ * P_ * A_.transpose() + Q_;
     //更新卡尔曼系数
-    K_ = P_bar * (P_bar + R_).inverse();
+    K_ = P_bar * H_.transpose() * (H_ * P_bar * H_.transpose() + R_).inverse();
     //更新状态估计值
-    x_k = x_k_bar + K_ * (z_k - x_k_bar);
+    x_k = x_k_bar + K_ * (z_k - H_ * x_k_bar);
     //更新估计协方差
     MatrixXd I = MatrixXd::Identity(x_k.size(),x_k.size());
-    P_ = (I - K_) * P_bar;
+    P_ = (I - K_ * H_) * P_bar;
 }
 
 void Kalman::Predict(int l) {
-    VectorXd z_pre = x_k;
+    VectorXd z_pre = H_ * x_k; //将测量值作为第一次迭代的测量最优估计
     VectorXd x_pre = x_k;
     MatrixXd K_pre = K_;
     MatrixXd P_pre = P_;
     for(;l>0;l--){
         VectorXd x_pre_bar = A_ * x_pre;
         MatrixXd P_pre_bar = A_ * P_pre * A_.transpose() + Q_;
-        K_pre = P_pre_bar * (P_pre_bar + R_).inverse();
-        x_pre = x_pre_bar + K_pre * (z_pre - x_pre_bar);
+        K_pre = P_pre_bar * H_.transpose() * (H_ * P_pre_bar * H_.transpose() + R_).inverse();
+        x_pre = x_pre_bar + K_pre * (z_pre - H_ * x_pre_bar);
         MatrixXd I = MatrixXd::Identity(x_k.size(),x_k.size());
         P_pre = (I - K_pre) * P_pre_bar;
     }
