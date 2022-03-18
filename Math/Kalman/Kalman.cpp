4653028d0582674793ce823a35780c564e967a20
tyy <tshuai1410771022@gmail.com>
2022-03-18 22:44:27 +0800

    update kalman and predictor
    now it can initial a nine dimension state

--- Math/Kalman/Kalman.cpp
+++ Math/Kalman/Kalman.cpp
@@ -5,10 +5,16 @@ Kalman::Kalman() {}
 void Kalman::Init(int z_dim, int x_dim, float step) {
     H_ = MatrixXf::Identity(z_dim,x_dim); //初始化为 z_dim * x_dim 的标准矩阵
     A_ = MatrixXf::Identity(x_dim,x_dim); //初始化为 x_dim * x_dim 的标准矩阵
-    if(x_dim % z_dim == 0){
-        for(int i = 0; i < x_dim/2; i++){
+    if(x_dim / z_dim == 2){
+        for(int i = 0; i < z_dim; i++){
             A_(i,z_dim + i) = step;
         }
+    }else if(x_dim / z_dim == 3){
+        for(int i = 0; i < x_dim - z_dim; i++){
+            A_(i,z_dim + i) = step;
+        }
+        for(int i = 0; i < z_dim; i++)
+            A_(i,2*z_dim + i) = 0.5 * step * step;
     }
     P_ = MatrixXf::Identity(x_dim, x_dim);
     I = MatrixXf::Identity(x_dim,x_dim);
@@ -30,24 +36,27 @@ void Kalman::Update(VectorXf z_k) {
 
     //cout << "--- A Matrix ---" << endl << A_ << endl;
     //cout << "--- H Matrix ---" << endl << H_ << endl;
-    cout << "--- P Matrix ---" << endl << P_ << endl;
-    cout << "--- K Matrix ---" << endl << K_ << endl;
-    cout << "--- x estimate Vector ---" << endl << x_k << endl;
+    //cout << "--- P Matrix ---" << endl << P_ << endl;
+    //cout << "--- K Matrix ---" << endl << K_ << endl;
+    //cout << "--- x estimate Vector ---" << endl << x_k << endl;
 }
 
 void Kalman::Predict(int l) {
-    VectorXf z_pre = H_ * x_k; //将测量值作为第一次迭代的测量最优估计
+    VectorXf z_pre;
     VectorXf x_pre = x_k;
     MatrixXf K_pre = K_;
     MatrixXf P_pre = P_;
     for(;l>0;l--){
+        z_pre = H_ * x_pre; //将测量值作为第一次迭代的测量最优估计
         VectorXf x_pre_bar = A_ * x_pre;
         MatrixXf P_pre_bar = A_ * P_pre * A_.transpose() + Q_;
         K_pre = P_pre_bar * H_.transpose() * (H_ * P_pre_bar * H_.transpose() + R_).inverse();
         x_pre = x_pre_bar + K_pre * (z_pre - H_ * x_pre_bar);
-
-        P_pre = (I - K_pre) * P_pre_bar;
+        P_pre = (I - K_pre * H_) * P_pre_bar;
     }
+    x_l_k = x_pre;
+    //cout << " x_K : " << x_k.transpose() << endl;
+    //cout << " x_l : " << x_l_k.transpose() << endl;
 }
 
 void Kalman::reset() {}
